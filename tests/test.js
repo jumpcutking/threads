cbb6b1b172faca6225f53635af87faed4d787456
Jay <justinkaz@jumpcutking.com>
2023-05-15 21:12:25 -0400

    Added the test script - working with actions

---
+++ tests/test.js
@@ -0,0 +1,6 @@
+//For running the test through npn test
+//change the execution directory to this directory
+process.chdir(__dirname);
+require("./threads.js");
+
+//if the test is scucessful, the thread will close itself
